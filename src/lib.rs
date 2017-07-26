fbc6e8c78b4928954d3da2dfbc6dc04cf8624504
Laurent Colbois <laurent.colbois@gmail.com>
2017-07-26 16:40:19 +0200

    Added full LZ78 + Huffman compression

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,31 @@ mod huffman;
 use huffman::*;
 mod bitvec_util;
 mod lz_78;
+use lz_78::{lz78_coding,lz78_decoding};
 use bitvec_util::*;
 use std::collections::BTreeMap;
 
 
 pub fn compression(content: Vec<u8>) -> Vec<u8> {
-    let mut statistique: BTreeMap<u8, u64> = BTreeMap::new();
-    for item in content.iter() {
-        if statistique.contains_key(item){
-            *statistique.get_mut(item).unwrap() += 1;
-        } else {
-            statistique.insert(*item,1);
-        }
+    let lz78_coded : Vec<(u64,u8)> = lz78_coding(content.iter());
+
+    let (mut pointer_statistic, mut character_statistic) : (BTreeMap<u64,u64>,BTreeMap<u8,u64>) = (BTreeMap::new(), BTreeMap::new());
+
+    for item in lz78_coded.iter(){
+        *pointer_statistic.entry(item.0).or_insert(0) += 1;
+        *character_statistic.entry(item.1).or_insert(0) += 1;
     }
 
-    let tree = Node::from_statistics(&statistique);
+    let (pointer_tree, character_tree) = (Node::from_statistics(&pointer_statistic),Node::from_statistics(&character_statistic));
 
-    let mut output = tree.encode_tree_u8();
+    let mut output = BitVec::new();
+    output = append_bit_vec(output, &Node::encode_tree(&pointer_tree));
+    output = append_bit_vec(output, &Node::encode_tree(&character_tree));
 
-    let dictionnary = tree.to_dictionnary(BitVec::new());
-
-    for &character in content.iter() {
-        output = append_bit_vec(output, &dictionnary[&character]);
+    let (pointer_dictionnary, character_dictionnary) = (pointer_tree.to_dictionnary(BitVec::new()), character_tree.to_dictionnary(BitVec::new()));
+    for &(pointer, character) in lz78_coded.iter(){
+        output=append_bit_vec(output,&pointer_dictionnary[&pointer]);
+        output = append_bit_vec(output,&character_dictionnary[&character]);
     }
 
     serialize_bit_vec(&output)
@@ -36,12 +39,15 @@ pub fn decompression(content: Vec<u8>) -> Result<Vec<u8>, &'static str> {
     let input = deserialize_bit_vec(&content);
     let mut iter = input.iter();
 
-    let tree = Node::decode_tree_u8(&mut iter)?;
+    let (pointer_tree, character_tree) = (Node::decode_tree(&mut iter)?, Node::decode_tree(&mut iter)?);
 
-    let mut output: Vec<u8> = Vec::new();
-    while let Some(code) = tree.scan(&mut iter)? {
-        output.push(code);
+    let mut lz78_coded: Vec<(u64,u8)> = Vec::new();
+
+    while let Some(pointer_code) = pointer_tree.scan(&mut iter)? {
+        while let Some(character_code) = character_tree.scan(&mut iter)?{
+            lz78_coded.push((pointer_code,character_code));
+        }
     }
 
-    return Ok(output);
+    return lz78_decoding(lz78_coded.iter());
 }
